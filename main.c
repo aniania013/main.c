da90bc332e040ba84b1873f2c86cf2b4b85fefc7
aniania013 <100830565+aniania013@users.noreply.github.com>
2022-03-03 08:26:18 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define pi 3.14
+int main(void) {
+  int r = 8;
+  double pole;
+  pole = pi*r*r;
+  printf("Pole jest rowne: %f", pole);
+  return 0;
+}
+// fewfrewfe
+/*rgretg3g
+ergege
+erge */
+// Dowiedziec sie na czym polega scanf (wprowadzenie danych do programu). dokomczyc gihub. Zrobić przynajmniej 3 pierwsze zadania. 
